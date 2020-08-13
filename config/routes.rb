07bad8ece036419090b75018de98e1b205e3d194
Cara Bell <carabell1994@aim.com>
2020-08-13 15:54:34 +0100

    finished doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # POST "cocktails" = CREATE
 
   resources :cocktails, only: [:index, :show, :new, :create]
+  resources :doses, only: [:new, :create, :destroy]
 end
